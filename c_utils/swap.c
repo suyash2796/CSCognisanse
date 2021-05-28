f5a78fd5fa01d9795160a0c93f333517938bd1bb
Suyash Shrivastava <65887107+suyash2796@users.noreply.github.com>
2021-05-28 18:23:32 +0530

    Delete c_utils directory

--- c_utils/swap.c
+++
@@ -1,8 +0,0 @@
-/* Function to swap *a and *b */
-void swap(int *a, int *b)
-{
-
- int temp = *a;
-    *a = *b;
-    *b = temp;
-}
--- c_utils/utils.h
+++
@@ -1,4 +0,0 @@
-int min (int x, int y);
-int min (int x, int y);
-void swap(int *a, int *b);
-void print_arr(int *arr,int n);
