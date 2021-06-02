7528c367322f8690dabf2137aab8afbc08280b32
suyash shrivastava <suyash2796@gmail.com>
2021-06-02 16:58:44 +0530

    completing KMP

--- pattern_searching/KMP/substring_match.py
+++ pattern_searching/KMP/substring_match.py
@@ -2,7 +2,7 @@
 def substring_match(main_string, substring):
     pattern_table = make_pattern(substring)
     is_match = check_for_match(main_string, substring, pattern_table)
-    return True # return bool
+    return is_match # return bool
 
 def make_pattern(substring):
     pattern = [-1 for i in substring]
@@ -23,10 +23,24 @@ def make_pattern(substring):
     return pattern
 
 def check_for_match(main_string, substring, pattern_table):
-    # TODO: implement string match logic
-    return True
+    left=0
+    right=0
+    while right + len(substring) -left <= len(main_string):
+        if substring[left] == main_string[right]:
+            if left == len(substring) -1:
+                return True
+            left+=1
+            right+=1
+        elif left>0:
+            left = pattern_table[left-1] +1
+        else:
+            right+=1
+    return False
+
+
+
 
 if __name__ == '__main__':
-    print(substring_match('ababbababab', 'bbaba'))
+    print(substring_match('ababbababab', 'bbababs'))
 
 
