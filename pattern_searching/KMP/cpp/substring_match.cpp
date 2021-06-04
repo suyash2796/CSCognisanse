a75cd35c7b311ab0e4f260ea15bd3c931d2d035d
suyash shrivastava <suyash2796@gmail.com>
2021-06-04 10:48:43 +0530

    cpp kmp complete

--- pattern_searching/KMP/cpp/substring_match.cpp
+++ pattern_searching/KMP/cpp/substring_match.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
-
+using namespace std;
 
 int main(){
-    string str = 'ababababbabbbababa';
-    string substr = 'abba';
+    string str = "ababababbabbbababa";
+    string substr = "abbax";
     vector<int> pattern(substr.size(), -1);
     int pl = 0;
     int pr=1;
@@ -14,7 +13,7 @@ int main(){
         if (substr[pr]==substr[pl]){
             pattern[pr] = pl;
             pr++;
-            pj++;
+            pl++;
         }
         else if(pl>0)
             pl=pattern[pl-1]+1;
@@ -24,7 +23,23 @@ int main(){
         }
         
     }
-//todo
-    //implement match loop
+    int i=0;
+    int j=0;
+    while (i+substr.size()-j <= str.size())
+    {
+        if (substr[j]==str[i]){
+            if (j==substr.size()-1){
+                cout<<"substring matched"<<endl;
+                return 0;}
+            i++;
+            j++;
+
+        }
+        else if (j>0)
+            j = pattern[j-1] +1;
+        else
+            i++;
+    }
+    cout<<"substring not matched"<<endl;
     return 0;
 }
