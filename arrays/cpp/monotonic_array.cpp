438fe8dcf1072ef242d17e392f920cb7e89ca973
suyash shrivastava <suyash2796@gmail.com>
2021-06-05 13:50:21 +0530

    monontonic arr cpp col

--- arrays/cpp/monotonic_array.cpp
+++ arrays/cpp/monotonic_array.cpp
@@ -7,12 +7,28 @@
 
 //O(N) time and O(1) space solution
 
-#include<istream>
+#include<iostream>
 #include<vector>
 using namespace std;
 
 int main(){
     //todo
-
+    vector<int> arr = {1,2,3,-4,7,9,10};
+    int size = arr.size();
+    int i =1;
+    int j=1;
+    int first = arr[0];
+    for(int k=1;k<size;k++){
+        if(arr[k]>=first)
+            i++;
+        if(arr[k]<=first)
+            j++;
+        first = arr[k];
+    }
+    if(i==size || j==size)
+        cout<<"monotonic array"<<endl;
+    else
+        cout<<"Non-monotonic array"<<endl;    
+    
     return 0;
 }
