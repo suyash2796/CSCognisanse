463e2fe7e7305ff82aaace1fc9280bcba51ca00a
suyash shrivastava <suyash2796@gmail.com>
2021-06-14 23:01:39 +0530

    BST todo

---
+++ BST/cpp/bst_construction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class BST {
+    public:
+        BST *left;
+        BST *right;
+        int value ;
+
+}
+
+//TODO
---
+++ BST/python/bst_construction.py
@@ -0,0 +1,10 @@
+## BST - Binary Search Tree
+class BST:
+    def __init__(self, value):
+        '''init left node, right node and value'''
+        self.left = None
+        self.right = None
+        self.value = value
+
+#TODO
+#construct BST
