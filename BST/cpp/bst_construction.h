f40f2b68490c46e2a88c2f6528147e1519055be0
davidqdo <47126935+davidqdo@users.noreply.github.com>
2021-06-15 04:01:33 -0700

    Create bst_construction.h

---
+++ BST/cpp/bst_construction.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class BST {
+    public:
+        BST *left;
+        BST *right;
+        int value ;
+};
+
