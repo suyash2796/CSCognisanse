ef4ab46e612184d48d37b7e92e8441c9b72803f7
davidqdo <47126935+davidqdo@users.noreply.github.com>
2021-06-15 12:20:39 -0700

    Update main.py

--- BST/python/main.py
+++ BST/python/main.py
@@ -3,5 +3,5 @@ from src.bst_construction import *
 arr = [24, 8, 3, 14, 34, 87, 45, 23, 76, 23, 2, 5, 8]
  
 root = constructTree(arr)
-print "Inorder traversal of constructed BST:"
+print ("Inorder traversal of constructed BST:")
 printInorder(root)
