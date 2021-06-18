bfebe3051adf3389e36d2a44abfe824d493b1f4a
Shubham Jain <sm20mtech12007@iith.ac.in>
2021-06-18 10:53:11 +0530

    with Linked list and main method implementation

--- linked_lists/python/remove_nth_node_from_last.py
+++ linked_lists/python/remove_nth_node_from_last.py
@@ -1,3 +1,7 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         t = head
@@ -11,7 +15,25 @@ class Solution:
         head = dummy
         temp = head
         for i in range(n):
-            print(i)
             temp = temp.next
         temp.next = temp.next.next
         return head.next
+    
+if __name__ == '__main__':
+    a = ListNode(1)
+    b = ListNode(2)
+    c = ListNode(3)
+    d = ListNode(4)
+    e = ListNode(5)
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+    sol = Solution()
+    l = []
+    head = sol.removeNthFromEnd(a,n=2)
+    while head:
+      l.append(head.val)
+      head = head.next
+    print(l)
+    
