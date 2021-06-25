5128cfb7eafe6b8d1fdb8af645468fcb4d03f7da
suyash shrivastava <suyash2796@gmail.com>
2021-06-25 11:47:21 +0530

    naive cpp pattern match

--- pattern_searching/naive/cpp/naive_pattern_searching.cpp
+++ pattern_searching/naive/cpp/naive_pattern_searching.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 using namespace std;
 
-//todo
-
 int main(){
+    string main_str="abaabbbaaa";
+    string sub_str = "suy";
+    int l_main= main_str.length();
+    int l_sub= sub_str.length();
+    bool matched= false;
+    for (int i=0;i<=l_main-l_sub+1;i++){
+        int cnt=0;
+        for(int j=0;j<=l_sub;j++){
+            if(main_str[i+j]!=sub_str[j]){
+                break;
+            }
+            cnt++;
+        }
+        if(cnt==l_sub){
+            matched=true;
+        }
+    }
+    if (matched==true){
+        cout<<"sub string matched!";
+    }
+    else{
+        cout<<"sub string not matched!";
+    }
     return 0;
 }
